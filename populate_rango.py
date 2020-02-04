c63500211eee3c84ba84f01dfd04f99f0f74025e
Win <>
2020-02-04 20:15:31 +0000

    Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,9 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+    cats = {'Python': {'pages': python_pages, 'views': 32, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+            'Other Frameworks': {'pages': other_pages, 'views': 128, 'likes': 16} }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
